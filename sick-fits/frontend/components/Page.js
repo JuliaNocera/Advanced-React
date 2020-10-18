2ff24d0284348e89c039075ce778e8b0d5e120ff
JuliaNocera <nocera.julia@gmail.com>
2020-10-18 00:54:02 -0700

    chore(Page): Add StyledPage and Inner styled divs

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -4,49 +4,31 @@ import styled from 'styled-components'
 import Header from './Header'
 import Meta from './Meta'
 
-// We'll eventually add this to a different file so we can export and import them 
-const MyButton = styled.button`
+const StyledPage = styled.div`
+  background: white;
+  color: black;
+`
+
+const Inner = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 2rem;
   background: red;
-  font-size: ${(props) => props.huge ? '100px' : '50px'};
-
-  /* you can also pass a value in the prop */
-  color: ${(props) => props.color ? props.color : 'yellow'};
-
-  /* 
-    you can target an element inside the MyButton component by the tag, like 'span' 
-  */
-  span {
-    font-size: 100px;
-  }
-
-  /* You could add a classname if you want as well or instead */
-  .BigNerd {
-    font-size: 150px;
-  }
 `
 
 /*
   We will render components for things that need to show up on every page - eg: Header component
 */
-
 class Page extends Component {
   render() {
     return (
-      <div>
+      <StyledPage>
         <Meta />
         <Header />
-        <MyButton huge color="purple">
-          <span className="BigNerd">🤓</span>
-          Click me
-          <span>💩</span>
-        </MyButton>
-        <MyButton>
-          <span className="BigNerd">🤓</span>
-          Click me
-          <span>💩</span>
-        </MyButton>
-        {this.props.children}
-      </div>
+        <Inner>
+          {this.props.children}
+        </Inner>
+      </StyledPage>
     )
   }
 }
