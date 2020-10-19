8c4e57e87d7e6cf9404d468e7eef3fd6b9cf9f53
JuliaNocera <nocera.julia@gmail.com>
2020-10-18 20:06:54 -0700

    feat(GraphQL): Adding in-memory store for creating and querying 'dogs'

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const Mutations = {};
+const Mutations = {
+  createDog(parent, args, context, info) {
+    console.log(context)
+    global.dogs = global.dogs || [];
+    // write to db 
+    const newDog = { name: args.name }
+    global.dogs.push(newDog)
+    return newDog
+  }
+};
 
 module.exports = Mutations;
