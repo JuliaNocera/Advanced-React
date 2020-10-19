56940e89ec0ee3d7498eff517555fff7c07cbcd7
JuliaNocera <nocera.julia@gmail.com>
2020-10-18 21:24:00 -0700

    chore(_app): Sets up the ApolloProvider in the custom app component

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,20 +1,42 @@
 import App, { Container } from 'next/app'
+import { ApolloProvider } from 'react-apollo'
+import withData from '../lib/withData'
 
 import Page from '../components/Page'
 
-class MyApp extends App  {
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    /*
+      This is a special method from next.js 
+      that runs before the first render.
+      Whatever is returned from this function
+      goes onto props.
+    */
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    // This is neccessary for server side rendering
+    pageProps.query = ctx.query
+    return { pageProps }
+  }
+
   render() {
-    const { Component } = this.props
+    const { Component, apollo, pageProps } = this.props
 
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
-      </Container>      
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
+      </Container>
     )
-  }  
+  }
 }
 
-export default MyApp
+// Using withData makes the apollo client via this.props
+export default withData(MyApp)
 
