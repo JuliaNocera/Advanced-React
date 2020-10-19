fb58d6de1ea625c1d5633b2c8009f36a9265c50a
JuliaNocera <nocera.julia@gmail.com>
2020-10-18 20:49:51 -0700

    --amend

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -5,6 +5,7 @@ const Query = {
   /*
     Forwarding to the db is a great way to build out the initial functionality
     And then you can come back for the authentication and any more complex interactions in the resolvers
+    This is an alternative to the below implementation
   */
   // async items(parent, args, context, info) {
   //   console.log('items fetchtime')
