4caeb1be07f2e851a62cc341ef963d42242adcad
JuliaNocera <nocera.julia@gmail.com>
2020-10-18 20:48:58 -0700

    chore(ForwardToPrisma): use forwardTo from prisma to just pass the query through to the prisma api

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,10 +1,16 @@
+const { forwardTo } = require('prisma-binding');
 
 const Query = {
-  async items(parent, args, context, info) {
-    console.log('items fetchtime')
-    const items = await context.db.query.items()
-    return items
-  }
+  items: forwardTo('db')
+  /*
+    Forwarding to the db is a great way to build out the initial functionality
+    And then you can come back for the authentication and any more complex interactions in the resolvers
+  */
+  // async items(parent, args, context, info) {
+  //   console.log('items fetchtime')
+  //   const items = await context.db.query.items()
+  //   return items
+  // }
 };
 
 module.exports = Query;
